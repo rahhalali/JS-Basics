ad7d058c4e7c051b428c363c1852bfc63d6012fa
rahhal ali <alih.rahhal@hotmail.com>
2021-05-29 21:56:18 +0300

    step_1 completed

--- javascript-basics-02/step 1/step_1.js
+++ javascript-basics-02/step 1/step_1.js
@@ -1,10 +1,12 @@
-var Image_Id = document.querySelector('#image1');
 
-   function imagefun() {
+var Image_Id = document.querySelector('#image1');
+Image_Id.addEventListener("mouseover",()=>{
     if (Image_Id.src.match("images/image1.jpg")) {
         Image_Id.src = "images/image1_2.jpg";
     }
     else {
         Image_Id.src = "images/image1.jpg";
     }
-}
+
+})
+  
