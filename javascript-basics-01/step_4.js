3c8a66782750f873669aa3f19703f4fa22a5000e
rahhal ali <alih.rahhal@hotmail.com>
2021-05-29 18:14:54 +0300

    step_4 completed

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,8 @@
+var name = prompt("Enter your name");
+var surname =prompt("Enter your surname");
+var city=prompt("Enter your city name");
+
+function Info(name,surname,city){
+    window.alert("name is : "+name+"\nsurname is : "+surname+"\ncity is : "+city);
+}
+Info(name,surname,city);
