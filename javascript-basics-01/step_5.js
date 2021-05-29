1a7a213fbc32c70f1c5be92ab0f67b9b4f2116ff
rahhal ali <alih.rahhal@hotmail.com>
2021-05-29 20:04:20 +0300

    step_5 completed

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,7 @@
+
+var btn =document.getElementById("validate");
+btn.addEventListener("click",function multi(){
+    var first = document.getElementById("first_number").value;
+    var second = document.getElementById("second_number").value;
+    alert(first*second);
+});
