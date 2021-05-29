fa528686f77cc52e8ad7c1ee7d77082462eec622
rahhal ali <alih.rahhal@hotmail.com>
2021-05-29 21:00:47 +0300

    step_6 completed

---
+++ javascript-basics-01/step_6.js
@@ -0,0 +1,12 @@
+
+var btn=document.getElementById("validate");
+btn.addEventListener("click",function(){
+    var first= document.getElementById("first_number").value;
+    var second=document.getElementById("second_number").value;
+    window.alert(remainder(first,second));
+});
+function remainder(x,y){
+    var z= x/y;
+    var mod =x%y;
+    return mod;
+}
