3a59f466526e9d47240a1dc0873983ad4b56e3b8
rahhal ali <alih.rahhal@hotmail.com>
2021-05-30 04:41:44 +0300

    step_3 completed

---
+++ javascript-basics-03/step 3/step_3.js
@@ -0,0 +1,10 @@
+var text=document.getElementById("text");
+function green(){
+    text.style.color="green";
+}
+function red(){
+    text.style.color="red";
+}
+function blue(){
+    text.style.color="blue";
+}
